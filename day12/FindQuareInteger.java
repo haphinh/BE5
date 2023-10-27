011989fe32e0cacc0e541ee80f4b160a27d196d2
Ha Truong <148224126+haphinh@users.noreply.github.com>
2023-10-27 18:40:58 +1100

    Add files via upload

--- day12/FindQuareInteger.java
+++ day12/FindQuareInteger.java
@@ -4,14 +4,24 @@ public class FindQuareInteger {
 	public static void main(String[] args) {
 
 		int a = 1;
-		int b = 25;
-		int numberOfSquareInteger = findSquareInteger(a, b);
+		int b = 100;
+		int numberOfSquareInteger = countSquaresInteger(a, b);
 		System.out.println("the number of square integer in the range is : " + numberOfSquareInteger);
 	}
 
-	public static int findSquareInteger(int a, int b) {
-		
-		int numberOfSquareInteger = (int) (Math.floor(Math.sqrt(b)) - Math.ceil(Math.sqrt(a)) + 1);
-		return numberOfSquareInteger;
+	public static int countSquaresInteger(int a, int b) {
+
+		int count = 0;
+		int num = 1;
+
+		while (num * num < a) {
+			num++;
+		}
+
+		while (a <= num * num && num * num <= b) {
+			count++;
+			num++;
+		}
+		return count;
 	}
 }
